2a96b3102dd24e129904f4bf3452532b8fb16263
Logan Campbell <colin@logaan.net>
2015-12-31 17:13:44 +1100

    moves->coords

--- src/mmo_snake/core.cljs
+++ src/mmo_snake/core.cljs
@@ -1,5 +1,6 @@
 (ns mmo-snake.core
-  (:require [reagent.core :as r]))
+  (:require [reagent.core :as r]
+            [cljs.pprint :refer [pprint]]))
 
 (enable-console-print!)
 
@@ -13,9 +14,10 @@
     [:rect {:key (str x "," y)
             :width 10
             :height 10
-            :x x
-            :y y
-            :style {:fill (colors index)}}]))
+            :x (* x 10)
+            :y (* y 10)
+            :style {:fill (colors index)
+                    :fill-opacity 0.5}}]))
 
 (defn simple-component [state]
   [:svg {:width 500 :height 500}
@@ -26,8 +28,38 @@
   (r/render-component [simple-component state]
                       (.-body js/document)))
 
+(def blue-moves
+  [[[0 0] :east]
+   [[1 0] :south]
+   [[1 2] :east]])
+
+(def directions
+  {:east [1 0]
+   :west [-1 0]
+   :north [0 -1]
+   :south [0 1]})
+
+(defn move [[dx dy] [cx cy]]
+  [(+ cx dx) (+ cy dy)])
+
+(def red-coords
+  [[0 0] [1 0] [1 1] [1 2] [2 2] [3 2]])
+
+(defn infinite-lines [moves]
+  (for [[coord d] moves]
+    (take 10 (iterate (partial move (directions d)) coord))))
+
+(defn moves->coords [game-length moves]
+  (let [ilines (infinite-lines moves)
+        history (->> ilines
+                     (partition 2 1)
+                     (mapcat (fn [[line [bend & _]]]
+                               (take-while #(not= bend %) line))))
+        future (last ilines)]
+    (take game-length (concat history future))))
+
 (def state
-  (atom {:snakes [[[20 10] [10 0]]
-                  [[100 10] [110 10]]]}))
+  (atom {:snakes [red-coords
+                  (moves->coords 6 blue-moves)]}))
 
 (render-simple state)
