ecaf8013033069283266c8236d110c7c755ae7f8
Logan Campbell <colin@logaan.net>
2015-12-31 17:26:49 +1100

    correct snake length

--- src/mmo_snake/core.cljs
+++ src/mmo_snake/core.cljs
@@ -28,38 +28,39 @@
   (r/render-component [simple-component state]
                       (.-body js/document)))
 
-(def blue-moves
-  [[[0 0] :east]
-   [[1 0] :south]
-   [[1 2] :east]])
-
 (def directions
-  {:east [1 0]
-   :west [-1 0]
+  {:east  [1 0]
+   :west  [-1 0]
    :north [0 -1]
    :south [0 1]})
 
 (defn move [[dx dy] [cx cy]]
   [(+ cx dx) (+ cy dy)])
 
-(def red-coords
-  [[0 0] [1 0] [1 1] [1 2] [2 2] [3 2]])
-
 (defn infinite-lines [moves]
   (for [[coord d] moves]
     (take 10 (iterate (partial move (directions d)) coord))))
 
-(defn moves->coords [game-length moves]
+(defn moves->coords [game-length snake-length moves]
   (let [ilines (infinite-lines moves)
         history (->> ilines
                      (partition 2 1)
                      (mapcat (fn [[line [bend & _]]]
                                (take-while #(not= bend %) line))))
-        future (last ilines)]
-    (take game-length (concat history future))))
+        future (last ilines)
+        full-history (take game-length (concat history future))]
+    (drop (- game-length snake-length) full-history)))
+
+(def red-coords
+  [[0 0] [1 0] [1 1] [1 2] [2 2] [3 2]])
+
+(def blue-moves
+  [[[0 0] :east]
+   [[1 0] :south]
+   [[1 2] :east]])
 
 (def state
   (atom {:snakes [red-coords
-                  (moves->coords 6 blue-moves)]}))
+                  (moves->coords 8 6 blue-moves)]}))
 
 (render-simple state)
