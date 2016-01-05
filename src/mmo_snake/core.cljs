594253a2b54ce553d5c02a8fbdbb978f715793c9
Logan Campbell <colin@logaan.net>
2016-01-05 21:16:48 +1100

    move out view code

--- src/mmo_snake/core.cljs
+++ src/mmo_snake/core.cljs
@@ -1,31 +1,12 @@
 (ns mmo-snake.core
   (:require [reagent.core :as r]
             [cljs.pprint :refer [pprint]]
-            [mmo-snake.model.game :refer [moves->coords]]))
+            [mmo-snake.model.game :refer [moves->coords]]
+            [mmo-snake.view.game :refer [game]]))
 
 (enable-console-print!)
 
 (defn on-js-reload [] nil)
-
-(def colors
-  ["red" "blue" "green"])
-
-(defn snake [index coords]
-  (for [[x y] coords]
-    [:rect {:key (str x "," y)
-            :width 10
-            :height 10
-            :x (* x 10)
-            :y (* y 10)
-            :style {:fill (colors index)
-                    :fill-opacity 0.5}}]))
-
-(defn game [{:keys [snakes time]} {:keys [change-direction]}]
-  [:svg {:id :game :width 500 :height 500 :tabIndex 0
-         :style {:border "1px solid black"}
-         :onKeyDown change-direction}
-   (for [[index {:keys [length moves]}] (map list (range) snakes)]
-     (snake index (moves->coords time length moves)))])
 
 (def key-codes
   {65 [0 :ccw]
---
+++ src/mmo_snake/view/game.cljs
@@ -0,0 +1,22 @@
+(ns mmo-snake.view.game
+  (:require [mmo-snake.model.game :refer [moves->coords]]))
+
+(def colors
+  ["red" "blue" "green"])
+
+(defn snake [index coords]
+  (for [[x y] coords]
+    [:rect {:key (str x "," y)
+            :width 10
+            :height 10
+            :x (* x 10)
+            :y (* y 10)
+            :style {:fill (colors index)
+                    :fill-opacity 0.5}}]))
+
+(defn game [{:keys [snakes time]} {:keys [change-direction]}]
+  [:svg {:id :game :width 500 :height 500 :tabIndex 0
+         :style {:border "1px solid black"}
+         :onKeyDown change-direction}
+   (for [[index {:keys [length moves]}] (map list (range) snakes)]
+     (snake index (moves->coords time length moves)))])
