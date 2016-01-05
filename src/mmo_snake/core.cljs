148c05967bec8d582bfed2725308c1b4e6906de0
Logan Campbell <colin@logaan.net>
2016-01-05 21:31:40 +1100

    extracted controller and renamed composer fns

--- src/mmo_snake/core.cljs
+++ src/mmo_snake/core.cljs
@@ -1,33 +1,22 @@
 (ns mmo-snake.core
   (:require [reagent.core :as r]
             [cljs.pprint :refer [pprint]]
-            [mmo-snake.model.game :refer [add-move]]
+            [mmo-snake.controller.game :refer [actions]]
             [mmo-snake.view.game :refer [game]]))
 
 (enable-console-print!)
 
 (defn on-js-reload [] nil)
 
-(def key-codes
-  {65 [0 :ccw]
-   83 [0 :cw]
-   75 [1 :ccw]
-   76 [1 :cw]})
+(defn component [state]
+  (game @state (actions state)))
 
-(defn change-direction [state]
-  (fn [e]
-    (if-let [[player-no rotation] (key-codes e.keyCode)]
-      (swap! state add-move player-no rotation))))
-
-(defn simple-component [state]
-  (game @state {:change-direction (change-direction state)}))
-
-(defn render-simple [state]
-  (r/render-component [simple-component state] (.-body js/document))
+(defn start-game [state]
+  (r/render-component [component state] (.-body js/document))
   (js/setInterval (fn [] (swap! state update-in [:time] inc)) 100)
   (.focus (.getElementById js/document "game")))
 
-(render-simple
+(start-game
  (r/atom {:snakes [{:length 6
                     :moves [[[0 0] :east]
                             [[1 0] :south]
